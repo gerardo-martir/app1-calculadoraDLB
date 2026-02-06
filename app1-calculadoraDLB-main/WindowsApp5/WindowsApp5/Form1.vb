add3a526a56590f57730db5e4625748da87e8b91
gerardo-martir <gerardo.martir@iest.edu.mx>
2026-02-06 07:36:34 -0600

    calc con 4 botones
    calc con 4 botones, con la capacidad de sumar, restar, multiplicar o dividir 2 numeros.

---
+++ app1-calculadoraDLB-main/WindowsApp5/WindowsApp5/Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Numero1 As Integer = Integer.Parse(TextBox1.Text)
+        Dim Numero2 As Integer = Integer.Parse(TextBox2.Text)
+        Dim Resultado As Integer = Numero1 + Numero2
+        resultadolabel.Text = Resultado.ToString()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim Numero1 As Integer = Integer.Parse(TextBox1.Text)
+        Dim Numero2 As Integer = Integer.Parse(TextBox2.Text)
+        Dim Resultado As Integer = Numero1 - Numero2
+        resultadolabel.Text = Resultado.ToString()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim Numero1 As Integer = Integer.Parse(TextBox1.Text)
+        Dim Numero2 As Integer = Integer.Parse(TextBox2.Text)
+        Dim Resultado As Integer = Numero1 * Numero2
+        resultadolabel.Text = Resultado.ToString()
+    End Sub
+End Class
